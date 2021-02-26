2b9f1d40adb480678c1347564016be00d652d797
carla regina <carlareginam10@gmail.com>
2021-02-26 20:32:41 -0300

    add templates

--- src/main/java/com/mballem/curso/boot/CursoSpringMvcApplication.java
+++ src/main/java/com/mballem/curso/boot/CursoSpringMvcApplication.java
@@ -1,4 +1,4 @@
-package com.example.curso.boot;
+package com.mballem.curso.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
